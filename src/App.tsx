7d6076fb83fadfcaf10fd8cabf5f40583a25c566
zhaoan <zhaoan2018@163.com>
2021-05-18 17:32:02 +0800

    123

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p>Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
